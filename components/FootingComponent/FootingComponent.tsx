7546112a2612c2c47d4ffacf9ff265c9204c6197
ek90 <erkan.kaban@cogniss.com>
2025-04-16 20:13:44 +1000

    #2

--- components/FootingComponent/FootingComponent.tsx
+++ components/FootingComponent/FootingComponent.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Mail, } from "lucide-react"
+import { Mail } from "lucide-react"
 import InstagramSvg from '../../public/InstagramSvg';
 import styles from './FootinComponent.module.css'
 
